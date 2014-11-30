c35bb1281511a37ca8c3b43ea27a1309f7ea7123
Carolyn Fischer <carolyn.fischer7@gmail.com>
2014-11-30 14:58:26 +0200

    development can use sqlite

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
 # Use postgresql as the database for Active Record
-gem 'pg'
+group :test, :production do
+  gem 'pg'
+end
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
@@ -58,3 +60,8 @@ gem 'yaml_db'
 
 # taps for migrating to psql
 gem 'taps'
+
+group :development do
+  gem 'sqlite3'
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116155934) do
+ActiveRecord::Schema.define(version: 20141123170857) do
 
   create_table "pictures", force: true do |t|
     t.datetime "created_at"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141116155934) do
     t.datetime "updated_at"
     t.string   "credits"
     t.integer  "cover_picture_id"
+    t.integer  "user_id"
   end
 
   create_table "taggings", force: true do |t|
